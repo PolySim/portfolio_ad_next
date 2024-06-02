0a7a24f8add592ab68fa3101c2b94eb789687ec2
Desdevises Simon <simon.desdevises@gmail.com>
2024-06-02 17:06:00 +0200

    Feat : About page

---
+++ src/app/apropos/page.tsx
@@ -0,0 +1,35 @@
+import { AboutType } from "@/model/aboutModel";
+import Image from "next/image";
+import AboutImg from "@/image/about.jpeg";
+
+const getBiography = async () => {
+  "use server";
+  return await fetch(`${process.env.API_URL}/api/about`, {
+    method: "GET",
+  }).then((res) => res.json() as Promise<AboutType>);
+};
+
+export default async function AboutPage() {
+  const biography = await getBiography();
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-16 w-full max-w-7xl mx-auto p-8 md:p-10">
+      <div className="md:sticky top-6 w-full h-max">
+        <Image src={AboutImg} alt="Angeline Desdevises" />
+      </div>
+      <div className="text-gray-600 font-helvetica">
+        <h1 className="text-gray-900 text-4xl md:text-6xl font-bold">
+          Biographie (fr)
+        </h1>
+        <p className="text-xs font-medium text-gray-600 mt-3 md:mt-5 leading-5">
+          {biography?.fr}
+        </p>
+        <h4 className="mt-6 text-lg md:text-2xl font-semibold">
+          Biography (en)
+        </h4>
+        <p className="text-xs font-medium text-gray-600 mt-3 leading-5">
+          {biography?.en}
+        </p>
+      </div>
+    </div>
+  );
+}
