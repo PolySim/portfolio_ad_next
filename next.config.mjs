481cf8612bd38eb19f5f332150097704d95c02a8
Desdevises Simon <simon.desdevises@gmail.com>
2024-06-28 18:17:10 +0200

    fix : url api image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['angelinedesdevises.fr', '127.0.0.1'],
+        domains: ['api.angelinedesdevises.fr', '127.0.0.1'],
     },
 };
 
