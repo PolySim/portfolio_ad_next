0cfa3f1696ad00e77bd53c7f887ec61f881a303b
Desdevises Simon <simon.desdevises@gmail.com>
2024-07-14 16:05:15 +0200

    feat : add vps to send image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['api.angelinedesdevises.fr', '127.0.0.1'],
+        domains: ['api.angelinedesdevises.fr', '127.0.0.1', '217.160.24.233', '217.160.24.233:5066'],
     },
 };
 
