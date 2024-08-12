4c427af60e186c5fb38c8285f135a7d0217c71da
Desdevises Simon <simon.desdevises@gmail.com>
2024-08-12 22:04:24 +0200

    fix: image height

--- src/ui/Portfolio/ImageContainer.tsx
+++ src/ui/Portfolio/ImageContainer.tsx
@@ -12,7 +12,7 @@ const ImageContainer = ({
   return (
     <div
       onClick={() => setImageIndexOpen(index)}
-      className="group relative w-full aspect-video h-full cursor-pointer hover:scale-[0.98] transition duration-150"
+      className="group relative w-full aspect-video cursor-pointer hover:scale-[0.98] transition duration-150"
     >
       {children}
     </div>
