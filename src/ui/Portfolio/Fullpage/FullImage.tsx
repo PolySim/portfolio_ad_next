b0fbbce416ebe9215f70c20ad04a537ca8e84139
Desdevises Simon <simon.desdevises@gmail.com>
2024-08-29 19:11:17 +0200

    fix : fullpage image load

--- src/ui/Portfolio/Fullpage/FullImage.tsx
+++ src/ui/Portfolio/Fullpage/FullImage.tsx
@@ -1,29 +1,15 @@
 import Image from "next/image";
 import { ImageType } from "@/app/portfolio/[pageId]/page";
-import probe from "probe-image-size";
 
 const FullImage = async ({ image }: { image: ImageType }) => {
-  const ImageSrc = await fetch(
-    `${process.env.API_URL}/api/images/${image.id}`,
-  ).then((res) => res.blob());
-  let height = 300;
-  let width = 400;
-  if (ImageSrc.type.startsWith("image")) {
-    const { width: widthResult, height: heightResult } = await probe(
-      `${process.env.API_URL}/api/images/${image.id}`,
-    );
-    height = heightResult;
-    width = widthResult;
-  }
-
   return (
     <div className="h-screen w-screen snap-center min-w-screen">
       <Image
         className="object-contain h-full w-full"
         src={`${process.env.API_URL}/api/images/${image.id}`}
         alt={`image${image.id}`}
-        width={width}
-        height={height}
+        width={1920}
+        height={1080}
       />
     </div>
   );
