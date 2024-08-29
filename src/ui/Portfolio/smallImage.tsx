fcb3fb1714c66527350941075d015eb21d58356c
Desdevises Simon <simon.desdevises@gmail.com>
2024-08-29 19:06:54 +0200

    fix : small image load

--- src/ui/Portfolio/smallImage.tsx
+++ src/ui/Portfolio/smallImage.tsx
@@ -1,47 +1,36 @@
+"use client";
+
 import { ImageType } from "@/app/portfolio/[pageId]/page";
 import Image from "next/image";
 import ImageContainer from "@/ui/Portfolio/ImageContainer";
-import probe from "probe-image-size";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
-const SmallImage = async ({
-  image,
-  index,
-}: {
-  image: ImageType;
-  index: number;
-}) => {
-  const ImageSrc = await fetch(
-    `${process.env.API_URL}/api/images/${image.id}`,
-  ).then((res) => res.blob());
-  let height = 300;
-  let width = 400;
-  if (ImageSrc.type.startsWith("image")) {
-    const { width: widthResult, height: heightResult } = await probe(
-      `${process.env.API_URL}/api/images/${image.id}`,
-    );
-    height = heightResult;
-    width = widthResult;
-  }
+const SmallImage = ({ image, index }: { image: ImageType; index: number }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
 
   return (
     <ImageContainer index={index}>
-      <div className="w-full" style={{ aspectRatio: `${width}/${height}` }} />
+      {!isLoaded && (
+        <Image
+          src={`${process.env.NEXT_PUBLIC_API_URL}/api/images/${image.id}/blur`}
+          alt={`image_${image.id}`}
+          width={500}
+          height={325}
+          priority
+          className="w-full"
+        />
+      )}
       <Image
-        src={`${process.env.API_URL}/api/images/${image.id}/blur`}
-        alt={`image_${image.id}`}
-        width={width}
-        height={height}
-        priority
-        className="absolute top-0 left-0 w-full"
-        style={{ aspectRatio: `${width}/${height}` }}
-      />
-      <Image
-        src={`${process.env.API_URL}/api/images/${image.id}`}
+        src={`${process.env.NEXT_PUBLIC_API_URL}/api/images/${image.id}`}
         alt={`image_${image.id}`}
         width={500}
         height={325}
+        onLoadingComplete={() => setIsLoaded(true)}
         loading="lazy"
-        className="absolute top-0 left-0 w-full h-auto"
+        className={cn("w-full h-auto", {
+          "absolute top-0 left-0 -z-10": !isLoaded,
+        })}
       />
       {image.description && image.description !== "" && (
         <span className="flex justify-center bg-white/50 w-full text-center py-2 opacity-0 group-hover:opacity-100 transition -translate-y-1/2 group-hover:-translate-y-full backdrop-blur-md">
