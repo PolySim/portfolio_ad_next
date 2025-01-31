e81025db9dd7ab6cb3256fe4b26a1666aacd1fb1
Desdevises Simon <simon.desdevises@gmail.com>
2025-01-31 16:47:17 +0100

    to v4

--- src/ui/Admin/Ordre_des_reportages/Report.tsx
+++ src/ui/Admin/Ordre_des_reportages/Report.tsx
@@ -2,7 +2,7 @@ import { ReportType } from "@/model/reportModel";
 
 const ReportOrder = ({ report }: { report: ReportType }) => {
   return (
-    <div className="rounded-lg shadow p-6 bg-white cursor-grab">
+    <div className="rounded-lg shadow-sm p-6 bg-white cursor-grab">
       <h2 className="text-2xl font-helvetica">{report.title}</h2>
     </div>
   );
--- src/ui/Portfolio/Fullpage/FullArticle.tsx
+++ src/ui/Portfolio/Fullpage/FullArticle.tsx
@@ -1,7 +1,7 @@
 const FullArticle = ({ article }: { article: string }) => {
   return (
     <div className="flex justify-center items-center max-h-screen w-screen my-12 snap-center min-w-screen">
-      <p className="w-10/12 h-fit max-h-full p-12 text-base text-gray-700 shadow overflow-y-scroll rounded-lg leading-7 bg-white">
+      <p className="w-10/12 h-fit max-h-full p-12 text-base text-gray-700 shadow-sm overflow-y-scroll rounded-lg leading-7 bg-white">
         {article}
       </p>
     </div>
